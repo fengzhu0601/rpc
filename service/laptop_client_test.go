da7b851ecb0e976e96003ab3056acfd83c3b7bf0
fengzhu <125384977@qq.com>
2022-09-27 02:08:12 +0800

    implement unary gRPC

---
+++ service/laptop_client_test.go
@@ -0,0 +1,68 @@
+package service_test
+
+import (
+	"context"
+	"github.com/fengzhu0601/rpc/pb"
+	"github.com/fengzhu0601/rpc/sample"
+	"github.com/fengzhu0601/rpc/serializer"
+	"github.com/fengzhu0601/rpc/service"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"net"
+	"testing"
+)
+
+func TestClientCreateLaptop(t *testing.T) {
+	t.Parallel()
+
+	laptopServer, serverAddress := startTestLaptopServer(t)
+	laptopClient := newTestLaptopClient(t, serverAddress)
+
+	laptop := sample.NewLaptop()
+	expectedID := laptop.Id
+	req := &pb.CreateLaptopRequest{
+		Laptop: laptop,
+	}
+	res, err := laptopClient.CreateLaptop(context.Background(), req)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Equal(t, expectedID, res.Id)
+
+	// 检查是否保存到商店
+	other, err := laptopServer.Store.Find(res.Id)
+	require.NoError(t, err)
+	require.NotNil(t, other)
+
+	// 检查保存的laptop和发送的laptop是否相同
+	requireSameLaptop(t, laptop, other)
+}
+
+func startTestLaptopServer(t *testing.T) (*service.LaptopServer, string) {
+	laptopServer := service.NewLaptopServer(service.NewInMemoryLaptopStore())
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
+
+	listener, err := net.Listen("tcp", ":0") // 随机一个有效端口
+	require.NoError(t, err)
+
+	go grpcServer.Serve(listener)
+
+	return laptopServer, listener.Addr().String()
+}
+
+func newTestLaptopClient(t *testing.T, serverAddress string) pb.LaptopServiceClient {
+	conn, err := grpc.Dial(serverAddress, grpc.WithTransportCredentials(insecure.NewCredentials())) // 不检查安全性
+	require.NoError(t, err)
+	return pb.NewLaptopServiceClient(conn)
+}
+
+func requireSameLaptop(t *testing.T, laptop1, laptop2 *pb.Laptop) {
+	json1, err := serializer.ProtobufToJson(laptop1)
+	require.NoError(t, err)
+
+	json2, err := serializer.ProtobufToJson(laptop2)
+	require.NoError(t, err)
+	require.Equal(t, json1, json2)
+}
---
+++ service/laptop_server_test.go
@@ -0,0 +1,88 @@
+package service_test
+
+import (
+	"context"
+	"github.com/fengzhu0601/rpc/pb"
+	"github.com/fengzhu0601/rpc/sample"
+	"github.com/fengzhu0601/rpc/service"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"testing"
+)
+
+func TestServerCreateLaptop(t *testing.T) {
+	t.Parallel()
+
+	laptopNoId := sample.NewLaptop()
+	laptopNoId.Id = ""
+
+	laptopInvalidID := sample.NewLaptop()
+	laptopInvalidID.Id = "invalid-uuid"
+
+	laptopDuplicateID := sample.NewLaptop()
+	storeDuplicateID := service.NewInMemoryLaptopStore()
+	err := storeDuplicateID.Save(laptopDuplicateID)
+	require.Nil(t, err)
+
+	testCases := []struct {
+		name   string
+		laptop *pb.Laptop
+		store  service.LaptopStore
+		code   codes.Code
+	}{
+		{
+			name:   "success_with_id",
+			laptop: sample.NewLaptop(),
+			store:  service.NewInMemoryLaptopStore(),
+			code:   codes.OK,
+		},
+		{
+			name:   "success_no_id",
+			laptop: laptopNoId,
+			store:  service.NewInMemoryLaptopStore(),
+			code:   codes.OK,
+		},
+		{
+			name:   "success_invalid_id",
+			laptop: laptopInvalidID,
+			store:  service.NewInMemoryLaptopStore(),
+			code:   codes.InvalidArgument,
+		},
+		{
+			name:   "success_with_id",
+			laptop: laptopDuplicateID,
+			store:  storeDuplicateID,
+			code:   codes.AlreadyExists,
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			req := &pb.CreateLaptopRequest{
+				Laptop: tc.laptop,
+			}
+			server := service.NewLaptopServer(tc.store)
+			res, err := server.CreateLaptop(context.Background(), req)
+			if tc.code == codes.OK {
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				require.NotEmpty(t, res.Id)
+				if len(tc.laptop.Id) > 0 {
+					require.Equal(t, tc.laptop.Id, res.Id)
+				}
+			} else {
+				require.Error(t, err)
+				require.Nil(t, res)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, tc.code, st.Code())
+			}
+		})
+	}
+
+}
